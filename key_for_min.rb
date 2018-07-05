22bbb4fc5712aa65bb547e93036285e608bdf844
Aubree Abril <aubree.abril@gmail.com>
2018-07-05 14:32:46 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -5,7 +5,7 @@ def key_for_min_value(name_hash)
   min_key = 0
   min_value = 0
   name_hash.each do |key, val|
-    if min_value == nil || val < min_value
+    if min_value == 0 || val < min_value
       min_value = val
       min_key = key
     end
